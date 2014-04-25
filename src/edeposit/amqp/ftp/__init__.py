6c9aa08a4ec29e77e3afbe302451de1ce420d61f
Bystroushaak <bystrousak@kitakitsune.org>
2014-04-25 18:13:52 +0200

    Added basic body of reactToAMQP message. CreateUser renamed to AddUser.

--- src/edeposit/amqp/ftp/__init__.py
+++ src/edeposit/amqp/ftp/__init__.py
@@ -14,12 +14,8 @@ import sh
 from settings import *
 
 
-#= Functions & objects ========================================================
-def reload_configuration():
-    sh.killall("-HUP", "proftpd", _ok_code=[0, 1])
-
-
-class CreateUser(namedtuple("CreateUser", ["username", "password"])):
+#= Requests ===================================================================
+class AddUser(namedtuple("AddUser", ["username", "password"])):
     pass
 
 
@@ -36,5 +32,39 @@ class ListRegisteredUsers(namedtuple("ListRegisteredUsers", [])):
     pass
 
 
-def reactToAMQPMessage(message, uuid):
-    pass
+#= Responses ==================================================================
+
+
+
+#= Functions & objects ========================================================
+def reload_configuration():
+    sh.killall("-HUP", "proftpd", _ok_code=[0, 1])
+
+def _instanceof(instance, class_):
+    """Check type by matching ``.__name__``."""
+    return type(instance).__name__ == class_.__name__
+
+
+#= Main function ==============================================================
+def reactToAMQPMessage(message, UUID):
+    """
+    React to given (AMQP) message. `message` is usually expected to be
+    :py:func:`collections.namedtuple` structure filled with all necessary data.
+
+    Args:
+        message (.. class): TODO: ..
+
+        UUID (str):                unique ID of received message
+
+    Returns:
+        : response TODO: comment when the protocol will be ready
+
+    Raises:
+        ValueError: if bad type of `message` structure is given.
+    """
+    if _instanceof(message, CreateUser):
+        pass  # TODO: ..
+
+    raise ValueError(
+        "Unknown type of request: '" + str(type(message)) + "'!"
+    )
