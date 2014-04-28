bd93b0effc8828ad67ab5ae78808b6278939a41e
Bystroushaak <bystrousak@kitakitsune.org>
2014-04-28 16:15:05 +0200

    process_file() changed to iterator.
    Added some comments, sketch of process_request().

--- src/edeposit/amqp/ftp/proftpd_monitor.py
+++ src/edeposit/amqp/ftp/proftpd_monitor.py
@@ -9,6 +9,8 @@ import os.path
 
 import sh
 
+from __init__ import ImportRequest, SendEmail
+
 
 #= Variables ==================================================================
 #= Functions & objects ========================================================
@@ -20,6 +22,10 @@ def _read_stdin():
 
 
 def _parse_line(line):
+    # typical line looks like this;
+    # /home/ftp/xex/asd bsd.dat, xex, STOR, 1398351777
+    # filename may contain ',' character, so I am rsplitting the line from the
+    # end to the beginning
     line, timestamp = line.rsplit(",", 1)
     line, command = line.rsplit(",", 1)
     path, username = line.rsplit(",", 1)
@@ -32,14 +38,23 @@ def _parse_line(line):
     }
 
 
-def process_request(parsed, callback=None):
-    if not callback:
-        print parsed
-    else:
-        callback(parsed["path"])  # TODO: define the protocol of communication
+def process_request(parsed):
+    if not os.path.exists(parsed["path"]):
+        return
+
+    data = ""
+    with open(parsed["path"]) as f:
+        data = f.read()
+
+    if not data.strip():
+        return SendEmail(
+            username=parsed["username"],
+            subject="Blank export data",
+            text=""
+        )
 
 
-def process_file(file_iterator, callback=None):
+def process_file(file_iterator):
     for line in file_iterator:
         if "," not in line or "[" in line:  # TODO: remove [ check
             continue
@@ -52,15 +67,19 @@ def process_file(file_iterator, callback=None):
         if not os.path.exists(parsed["path"]):
             continue
 
-        process_request(parsed, callback)
+        yield process_request(parsed)
 
 
 #= Main program ===============================================================
 if __name__ == '__main__':
     try:
+        it = None
         if len(sys.argv) > 1:
-            process_file(sh.tail("-f", sys.argv[1], _iter=True))
+            it = process_file(sh.tail("-f", sys.argv[1], _iter=True))
         else:
-            process_file(_read_stdin())
+            it = process_file(_read_stdin())
+
+        for request in it:
+            print request
     except KeyboardInterrupt:
         sys.exit(0)
