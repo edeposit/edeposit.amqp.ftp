126a8c7488dc48a5eb543eafc0ece952159b3e80
Bystroushaak <bystrousak@kitakitsune.org>
2014-05-13 14:11:53 +0200

    Added (untested) sketch of meta decoding.

--- src/edeposit/amqp/ftp/decoders/__init__.py
+++ src/edeposit/amqp/ftp/decoders/__init__.py
@@ -4,30 +4,39 @@
 # Interpreter version: python 2.7
 #
 #= Imports ====================================================================
+import fields
 
+import parser_csv
+import parser_xml
+import parser_yaml
+import parser_json
+
+from meta_exceptions import MetaParsingException
 
 
 #= Variables ==================================================================
-SUPPORTED_FILES = [
-    "csv",
-    "json",
-    "xml",
-    "yaml"
-]
+SUPPORTED_FILES = {
+    "csv": parser_csv.decode,
+    "json": parser_json.decode,
+    "xml": parser_xml.decode,
+    "yaml": parser_yaml.decode
+}
 
 
 #= Functions & objects ========================================================
-class MetaParsingException(UserWarning):
-    """docstring for MetaParsingException"""
-    def __init__(self, value):
-        super(MetaParsingException, self).__init__(value)
-        self.value = value
+def parse_meta(fn, data):
+    if "." not in fn:
+        raise MetaParsingException(
+            "Can't recognize type of your metadata ('%s')!" % fn
+        )
 
+    suffix = fn.rsplit(".", 1)[1].lower()
 
-def parse_meta(data):
-    return "parsed data"
+    if suffix not in SUPPORTED_FILES:
+        raise MetaParsingException("Can't parse file of type '%s'!" % suffix)
 
+    parser = fields.FieldParser()
+    for key, val in SUPPORTED_FILES[suffix](data).items():
+        parser.process(key, val)
 
-#= Main program ===============================================================
-if __name__ == '__main__':
-    pass
+    return parser.get_epublication()
