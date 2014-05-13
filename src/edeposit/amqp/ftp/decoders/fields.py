40cc498b2619fc437a850dcb521f1542c129c8a1
Bystroushaak <bystrousak@kitakitsune.org>
2014-05-13 13:10:59 +0200

    fields.py: Changed the way how the fields are validated.

--- src/edeposit/amqp/ftp/decoders/fields.py
+++ src/edeposit/amqp/ftp/decoders/fields.py
@@ -8,22 +8,58 @@
 
 
 #= Variables ==================================================================
-REQUIRED_FIELDS = [
-    "ISBN",
-    "Vazba/forma",
-    "Název",
-    "Url (pouze pro online publikace)",
-    "Formát (pouze pro online publikace)",
-    "Místo vydání",
-    "Nakladatel",
-    "Měsíc a rok vydání",
-    "Pořadí vydání",
-    "Zpracovatel záznamu"  # TODO: look only for the first word
-]
-
-
 #= Functions & objects ========================================================
+class Field:
+    def __init__(self, keyword, description):
+        self.keyword = keyword
+        self.description = description
+        self.value = None
 
+    def check(self, key, value):
+        if self.keyword in key.lower().strip():
+            self.value = value
+            return True
+
+        return False
+
+    def is_valid(self):
+        return self.value is not None
+
+
+class FieldValidator:
+    def __init__(self):
+        self.fields = [
+            Field(keyword="isbn", description="ISBN"),
+            Field(keyword="vazba", description="Vazba/forma"),
+            Field(keyword="nazev", description="Název"),
+            Field(keyword="misto", description="Místo vydání"),
+            Field(keyword="nakladatel", description="Nakladatel"),
+            Field(keyword="datum", description="Měsíc a rok vydání"),
+            Field(keyword="poradi", description="Pořadí vydání"),
+            Field(keyword="zpracovatel", description="Zpracovatel záznamu")
+        ]
+
+        self.optional = [
+            Field(keyword="url", description="Url"),
+            Field(keyword="format", description="Formát"),
+            Field(keyword="podnazev", description="Podnázev"),
+            Field(keyword="cena", description="Cena"),
+        ]
+
+    def process(self, key, val):  # TODO: rename to add()?
+        for field in self.fields:
+            if field.check(key, val):
+                return
+
+        for field in self.optional:
+            if field.check(key, val):
+                return
+
+    def is_valid(self):
+        raise NotImplementedError("Not implemented yet.")
+
+    def get_epublication(self):
+        raise NotImplementedError("Not implemented yet.")
 
 
 #= Main program ===============================================================
