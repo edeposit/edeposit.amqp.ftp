27c9ebfe07ec5f9b630a0e0fc2edfcd34bf3747d
Bystroushaak <bystrousak@kitakitsune.org>
2014-05-14 14:20:26 +0200

    validator.py renamed to parser.py. Implemented structural validation.

--- src/edeposit/amqp/ftp/decoders/parser.py
+++ src/edeposit/amqp/ftp/decoders/parser.py
@@ -18,14 +18,46 @@ ALLOWED_TYPES = [
 
 
 #= Functions & objects ========================================================
+def _all_correct_list(array):
+    for item in array:
+        if not type(item) in [list, tuple]:
+            return False
+
+        if len(item) != 2:
+            return False
+
+    return True
+
+
+def convert_to_dict(data):
+    if isinstance(data, dict):
+        return data
+
+    if isinstance(data, list) or isinstance(data, tuple):
+        if _all_correct_list(data):
+            return dict(data)
+        else:
+            data = zip(data[::2], data[1::2])
+            return dict(data)
+    else:
+        raise MetaParsingException(
+            "Can't decode provided metadata - unknown structure."
+        )
+
+
 def check_structure(data):
     """
     Check whether the structure is flat dictionary.
     """
     if not isinstance(data, dict):
-        raise MetaParsingException(
-            "Data format has invalid strucure (dictionary is expected)."
-        )
+        try:
+            data = convert_to_dict(data)
+        except MetaParsingException, e:
+            raise
+        except:
+            raise MetaParsingException(
+                "Metadata format has invalid strucure (dictionary is expected)."
+            )
 
     for key, val in data.iteritems():
         if type(key) not in ALLOWED_TYPES:
