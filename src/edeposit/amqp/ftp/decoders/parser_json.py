fac9efd842445ec3df4b353912efffef853a73a9
Bystroushaak <bystrousak@kitakitsune.org>
2014-05-14 16:09:17 +0200

    parser_json.py tested and looks like it works just fine.

--- src/edeposit/amqp/ftp/decoders/parser_json.py
+++ src/edeposit/amqp/ftp/decoders/parser_json.py
@@ -15,9 +15,40 @@ def decode(data):
     decoded = None
     try:
         decoded = json.loads(data)
-    except:
-        raise MetaParsingException("Can't parse your JSON data.")
+    except Exception, e:
+        raise MetaParsingException("Can't parse your JSON data: %s" % e.message)
 
     decoded = parser.check_structure(decoded)
 
     return decoded
+
+
+def test_decoder():
+    def test_assertions(test_str):
+        data = decode(test_str)
+        assert data["isbn"] == "80-86056-31-7"
+        assert data["vazba"] == u"pevná"
+
+    # dict
+    test_str = """
+    {
+        "isbn": "80-86056-31-7",
+        "vazba": "pevná"
+    }
+    """
+    test_assertions(test_str)
+
+    # array
+    test_str = """
+    [
+        "isbn", "80-86056-31-7",
+        "vazba", "pevná"
+    ]
+    """
+    test_assertions(test_str)
+
+    try:
+        decode('[["isbn"], "80-86056-31-7", "asd"]')
+        raise AssertionError("Field check failed!")
+    except MetaParsingException:
+        pass
