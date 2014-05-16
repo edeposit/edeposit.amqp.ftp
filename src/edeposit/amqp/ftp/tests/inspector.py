796b3868afcccd67b97f24e6fcccd7bdd9089d30
Bystroushaak <bystrousak@kitakitsune.org>
2014-05-16 15:36:47 +0200

    tests/inspector.py filled with some usable content.

--- src/edeposit/amqp/ftp/tests/inspector.py
+++ src/edeposit/amqp/ftp/tests/inspector.py
@@ -4,7 +4,14 @@
 # Interpreter version: python 2.7
 #
 #= Imports ====================================================================
+import random
+import string
+import os.path
 
+import edeposit.amqp.ftp as ftp
+import edeposit.amqp.ftp.api as api
+import edeposit.amqp.ftp.monitor as monitor
+import edeposit.amqp.ftp.initializer as initializer
 
 
 #= Variables ==================================================================
@@ -15,7 +22,13 @@ BASE_PATH = os.path.dirname(__file__)
 class Inspector(object):
     ROBOT_LIBRARY_SCOPE = 'TEST SUITE'
 
+    def call(self, fn, *args, **kwargs):
+        return fn(*args, **kwargs)
 
-#= Main program ===============================================================
-if __name__ == '__main__':
-    pass
+    def get_random_name(self, size=8):
+        out = ""
+        s = string.ascii_lowercase
+        for i in range(size):
+            out += s[random.randint(0, len(s)-1)]
+
+        return out
