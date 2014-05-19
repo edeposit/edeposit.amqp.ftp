df96a540fa4729ff27922319f771b37803863868
Bystroushaak <bystrousak@kitakitsune.org>
2014-05-19 17:25:18 +0200

    Added some basic unittests for monitor.py.

---
+++ src/edeposit/amqp/ftp/tests/unittests/test_monitor.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+#= Imports ====================================================================
+import pytest
+
+import edeposit.amqp.ftp.monitor as monitor
+
+
+#= Variables ==================================================================
+
+
+
+#= Functions & objects ========================================================
+def test_parse_line():
+    l = monitor._parse_line("/path/to/the/he,ll, pcxaioay, EXIT, 1400508413")
+
+    assert l["timestamp"] == "1400508413"
+    assert l["command"] == "EXIT"
+    assert l["username"] == "pcxaioay"
+    assert l["path"] == "/path/to/the/he,ll"
+
+    with pytest.raises(ValueError):
+        monitor._parse_line("/path/to/the/hell, EXIT, 1400508413")
+
+def test_recursive_chmod():
+    raise NotImplementedError()
+
+def test_just_name():
+    assert monitor._just_name("/home/bystrousak/xex.asd") == "xex"
+    assert monitor._just_name("/home/bystrousak/xex") == "xex"
+
+def test_same_named():
+    out = monitor._same_named(
+        "/hello/joe.mp3",
+        [
+            "/hi/",
+            "/ola/how/are/you/joe.jpg",
+            "/ola/how/are/you/xex.jpg"
+        ]
+    )
+
+    assert out[0][0] == 1
+    assert out[0][1] == "/ola/how/are/you/joe.jpg"
