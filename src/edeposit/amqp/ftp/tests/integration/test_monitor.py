f30ed6257019b00ef1ea2660e986591e2e19b8a4
Bystroushaak <bystrousak@kitakitsune.org>
2014-05-20 13:13:45 +0200

    Added working FTP upload to the tests/integration/test_monitor.py.

--- src/edeposit/amqp/ftp/tests/integration/test_monitor.py
+++ src/edeposit/amqp/ftp/tests/integration/test_monitor.py
@@ -5,9 +5,9 @@
 #
 #= Imports ====================================================================
 import os
-import ftplib  # TODO: use ftputil (http://ftputil.sschwarzer.net)
 
 import pytest
+import ftputil
 from shared import get_random_str
 
 import edeposit.amqp.ftp.api as api
@@ -17,7 +17,6 @@ import edeposit.amqp.ftp.monitor as monitor
 #= Variables ==================================================================
 USERNAME = get_random_str()
 PASSWORD = get_random_str()
-ftp = None
 
 
 #= Tests ======================================================================
@@ -26,28 +25,26 @@ def setup_module(module):
 
 
 def teardown_module(module):
-    # api.remove_user(USERNAME)
+    api.remove_user(USERNAME)
+    api.remove_user("vbftgbjo")
     pass
 
 
-def test_upload():
-    global ftp
+def upload_files(path="src/edeposit/amqp/ftp/tests/integration/data"):
+    ftp = ftputil.FTPHost("localhost", USERNAME, PASSWORD)
 
-    ftp = ftplib.FTP("localhost")
-    ftp.login(USERNAME, PASSWORD)
-
-    log = []
-    path = "src/edeposit/amqp/ftp/tests/integration/data"
     for root, dirs, files in os.walk(path):
         for dn in dirs:
             full_dn = os.path.join(root, dn)
             remote_dn = full_dn[len(path):]
-            ftp.mkd(remote_dn)
-            # log.append("newd %s -> %s" % (full_dn, remote_dn))
+            ftp.makedirs(remote_dn)
 
         for fn in files:
             full_fn = os.path.join(root, fn)
             remote_fn = full_fn[len(path):]
-            log.append("newf %s -> %s" % (full_fn, remote_fn))
+            ftp.upload(full_fn, remote_fn)
 
-    raise AssertionError("\n".join(log))
+
+def test_monitor():
+    upload_files()
+
