17c3ab0f2c8712e37956f2fbf03511ddb17757b7
Bystroushaak <bystrousak@kitakitsune.org>
2014-05-21 15:11:10 +0200

    Implemented csv parser.
    Bump #10, #4.

--- src/edeposit/amqp/ftp/decoders/parser_csv.py
+++ src/edeposit/amqp/ftp/decoders/parser_csv.py
@@ -4,13 +4,64 @@
 # Interpreter version: python 2.7
 #
 #= Imports ====================================================================
+import csv
 
-
-
-#= Variables ==================================================================
-
+import parser
+from meta_exceptions import MetaParsingException
 
 
 #= Functions & objects ========================================================
+def remove_quotes(word):
+    """
+    Remove quotes from `word` if the word starts and ands with quotes (" or ').
+    """
+    if not word or len(word) <= 2:
+        return word
+
+    if word[0] == word[-1] and word[0] in ["'", '"']:
+        return word[1:-1]
+
+    return word
+
+
 def decode(data):
-    pass
+    # try to guess dialect of the csv file
+    dialect = None
+    try:
+        dialect = csv.Sniffer().sniff(data)
+    except Exception:
+        pass
+
+    # parse data with csv parser
+    handler = None
+    try:
+        data = data.splitlines()  # used later
+        handler = csv.reader(data, dialect)
+    except Exception, e:
+        raise MetaParsingException("Can't parse your CSV data: %s" % e.message)
+
+    # make sure, that data are meaningful
+    decoded = []
+    for cnt, line in enumerate(handler):
+        usable_data = filter(lambda x: x.strip(), line)
+
+        if not usable_data:
+            continue
+
+        if len(usable_data) != 2:
+            raise MetaParsingException(
+                "Bad number of elements - line %d:\n\t%s\n" % (cnt, data[cnt])
+            )
+
+        # remove trailing spaces, decode to utf-8
+        usable_data = map(lambda x: x.strip().decode("utf-8"), usable_data)
+
+        # remove quotes if the csv.Sniffer failed to decode right `dialect`
+        usable_data = map(lambda x: remove_quotes(x), usable_data)
+
+        decoded.append(usable_data)
+
+    # apply another checks to data
+    decoded = parser.check_structure(decoded)
+
+    return decoded
