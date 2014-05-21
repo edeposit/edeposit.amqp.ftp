bac5d5db922971d53d7eae48b1664b5c0745f160
Bystroushaak <bystrousak@kitakitsune.org>
2014-05-21 16:03:51 +0200

    Added tests for YAML decoder.
    Bump #16.

---
+++ src/edeposit/amqp/ftp/tests/unittests/test_decoders/test_parser_yaml.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+#= Imports ====================================================================
+import edeposit.amqp.ftp.decoders as decoders
+from edeposit.amqp.ftp.decoders import parser_yaml
+
+import pytest
+
+
+#= Tests ======================================================================
+def test_yaml():
+    def test_assertions(test_str):
+        data = parser_yaml.decode(test_str)
+        assert data["isbn"] == "80-86056-31-7"
+        assert data["vazba"] == u"pevná"
+
+    test_assertions(
+        """
+        isbn: 80-86056-31-7
+        vazba: pevná
+        """
+    )
+
+    test_assertions(
+        """
+        isbn:
+            80-86056-31-7
+        vazba:
+            pevná
+        """
+    )
+
+    with pytest.raises(decoders.MetaParsingException):
+        parser_yaml.decode('isbn: 80-86056-31-7: xex')
+
+    with pytest.raises(decoders.MetaParsingException):
+        parser_yaml.decode('isbn:')
+
+    with pytest.raises(decoders.MetaParsingException):
+        parser_yaml.decode('isbn')
