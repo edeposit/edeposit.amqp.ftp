e9aea5a4f8b81b5d8e3770f66dff291c25a247f6
Bystroushaak <bystrousak@kitakitsune.org>
2014-05-26 17:08:17 +0200

    Added basic reactions to __init__.py.

--- src/edeposit/amqp/ftp/__init__.py
+++ src/edeposit/amqp/ftp/__init__.py
@@ -3,19 +3,23 @@
 #
 # Interpreter version: python 2.7
 #
-#= Imports ====================================================================
-from settings import *
-from structures import *
+# Imports =====================================================================
+# from settings import *
+import api
+import structures
+import passwd_reader
 
 
-#= Variables ==================================================================
-#= Functions & objects ========================================================
-def _instanceof(instance, class_):
-    """Check type by matching ``.__name__``."""
-    return type(instance).__name__ == class_.__name__
+# Variables ===================================================================
+# Functions & objects =========================================================
+def _instanceof(instance, cls):
+    """
+    Check type of `instance` by matching ``.__name__`` with `cls.__name__`.
+    """
+    return type(instance).__name__ == cls.__name__
 
 
-#= Main function ==============================================================
+# Main function ===============================================================
 def reactToAMQPMessage(message, UUID):
     """
     React to given (AMQP) message. `message` is usually expected to be
@@ -32,8 +36,42 @@ def reactToAMQPMessage(message, UUID):
     Raises:
         ValueError: if bad type of `message` structure is given.
     """
-    if _instanceof(message, CreateUser):
-        pass  # TODO: ..
+    if _instanceof(message, structures.AddUser):
+        api.add_user(
+            username=message.username,
+            password=message.password
+        )
+        return True  # TODO: předělat na něco užitečnějšího? je to zapotřebí?
+    elif _instanceof(message, structures.RemoveUser):
+        api.remove_user(message.username)
+        return True
+    elif _instanceof(message, structures.ChangePassword):
+        api.change_password(
+            username=message.username,
+            new_password=message.new_password
+        )
+        return True
+    elif _instanceof(message, structures.ListRegisteredUsers):
+        return structures.Userlist(api.list_users())
+    elif _instanceof(message, structures.SetUserSettings):
+        username = message.username
+
+        # convert namedtuple to dict (python rocks)
+        conf_dict = dict(message._asdict())
+        del conf_dict["username"]
+
+        passwd_reader.save_user_config(
+            username,
+            conf_dict
+        )
+
+        conf_dict = passwd_reader.read_user_config(username)
+        conf_dict["username"] = username
+        return structures.UserSettings(**conf_dict)
+    elif _instanceof(message, structures.GetUserSettings):
+        conf_dict = passwd_reader.read_user_config(message.username)
+        conf_dict["username"] = message.username
+        return structures.UserSettings(**conf_dict)
 
     raise ValueError(
         "Unknown type of request: '" + str(type(message)) + "'!"
