b47fc8bea233516cf86bf85f0ebb158189c71ce7
Bystroushaak <bystrousak@kitakitsune.org>
2014-05-27 12:31:49 +0200

    Added tests of file structures:
    test_parse_MetadataFile()
    test_parse_EbookFile()
    test_parse_DataPair()
    
    Bump #16.

--- src/edeposit/amqp/ftp/tests/unittests/test_structures.py
+++ src/edeposit/amqp/ftp/tests/unittests/test_structures.py
@@ -3,19 +3,73 @@
 #
 # Interpreter version: python 2.7
 #
-#= Imports ====================================================================
+# Imports =====================================================================
 import pytest
 
+import edeposit.amqp.ftp.decoders as decoders
 import edeposit.amqp.ftp.structures as structures
 
 
-#= Variables ==================================================================
-#= Functions & objects ========================================================
+# Variables ===================================================================
+JSON_DATA = """
+[
+    "ISBN knihy", "80-86056-31-7",
+    "Vazba knihy", "brož.",
+    "Nazev knihy", "ZEN",
+    "Misto vydani", "Praha",
+    "Nakladatel", "Garda",
+    "Datum vydani", "09/2012",
+    "Poradi vydani", "1",
+    "Zpracovatel zaznamu", "Franta Putsalek"
+]
+"""
+
+
+# Functions & objects =========================================================
 def test_parse_MetadataFile():
-    raise NotImplementedError()
+    mf = structures.MetadataFile("filename.json", JSON_DATA)
+
+    assert mf.filename == "filename.json"
+    assert mf.raw_data == JSON_DATA
+    assert mf.parsed_data is None
+    assert mf.get_filenames() == ["filename.json"]
+
+    mf = mf.parse()
+
+    assert mf.parsed_data.ISBN == "80-86056-31-7"
+
+    mf = structures.MetadataFile("filename.csv", JSON_DATA)
+    with pytest.raises(decoders.MetaParsingException):
+        mf.parse()
+
 
 def test_parse_EbookFile():
-    raise NotImplementedError()
+    ef = structures.EbookFile("asd.data", "some_data")
+
+    assert ef.filename == "asd.data"
+    assert ef.raw_data == "some_data"
+    assert ef.get_filenames() == ["asd.data"]
+
 
 def test_parse_DataPair():
-    raise NotImplementedError()
+    dp = structures.DataPair(
+        structures.MetadataFile("filename.json", JSON_DATA),
+        structures.EbookFile("asd.data", "some_data")
+    )
+
+    # test .get_filenames()
+    assert len(dp.get_filenames()) == 2
+    assert "filename.json" in dp.get_filenames()
+    assert "asd.data" in dp.get_filenames()
+
+    assert dp.metadata_file.filename == "filename.json"
+    assert dp.ebook_file.filename == "asd.data"
+
+    assert dp.metadata_file.parsed_data is None
+
+    dp = dp.parse()
+
+    assert dp
+    assert dp.metadata_file.filename == "filename.json"
+    assert dp.metadata_file.parsed_data.ISBN == "80-86056-31-7"
+    assert dp.ebook_file.filename == "asd.data"
