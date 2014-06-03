63d22d3d0370fb9f3d4fcf6385ffceb178b91f9f
Bystroushaak <bystrousak@kitakitsune.org>
2014-06-03 18:19:55 +0200

    Added documentation for structures.py.
    Bump #15.

--- docs/api/ftp.structures.rst
+++ docs/api/ftp.structures.rst
@@ -1,7 +1,50 @@
 ftp.structures module
 =====================
-
 .. automodule:: ftp.structures
+
+
+Requests
+--------
+
+.. autoclass:: ftp.structures.AddUser
     :members:
-    :undoc-members:
-    :show-inheritance:
+
+.. autoclass:: ftp.structures.RemoveUser
+    :members:
+
+.. autoclass:: ftp.structures.ChangePassword
+    :members:
+
+.. autoclass:: ftp.structures.ListRegisteredUsers
+    :members:
+
+.. autoclass:: ftp.structures.SetUserSettings
+    :inherited-members:
+
+.. autoclass:: ftp.structures.GetUserSettings
+    :members:
+
+Responses
+---------
+
+.. autoclass:: ftp.structures.Userlist
+    :members:
+
+.. autoclass:: ftp.structures.UserSettings
+    :inherited-members:
+
+Import request
+--------------
+.. autoclass:: ftp.structures.ImportRequest
+    :members:
+
+File structures
++++++++++++++++
+.. autoclass:: ftp.structures.MetadataFile
+    :members:
+
+.. autoclass:: ftp.structures.EbookFile
+    :members:
+
+.. autoclass:: ftp.structures.DataPair
+    :members:
--- src/edeposit/amqp/ftp/structures.py
+++ src/edeposit/amqp/ftp/structures.py
@@ -3,6 +3,9 @@
 #
 # Interpreter version: python 2.7
 #
+"""
+This module contains all communication structures used in AMQP communication.
+"""
 # Imports =====================================================================
 from collections import namedtuple
 
@@ -12,38 +15,88 @@ import settings
 
 # Requests ====================================================================
 class AddUser(namedtuple("AddUser", ["username", "password"])):
+    """
+    Add new user to the ProFTPD server.
+
+    Args:
+        username (str): Alloed characters: ``a-zA-Z0-9._-``.
+        password (str): Password for the new user. Only hash is stored.
+    """
     pass
 
 
 class RemoveUser(namedtuple("RemoveUser", ["username"])):
+    """
+    Remove user from the ProFTPD server.
+
+    Args:
+        username (str): Alloed characters: ``a-zA-Z0-9._-``.
+    """
     pass
 
 
 class ChangePassword(namedtuple("ChangePassword", ["username",
                                                    "new_password"])):
+    """
+    Change password for the user.
+
+    Args:
+        username (str): Alloed characters: ``a-zA-Z0-9._-``.
+        new_password (str): New password for user.
+
+    """
     pass
 
 
 class ListRegisteredUsers(namedtuple("ListRegisteredUsers", [])):
+    """
+    List all registered users.
+
+    See also:
+        :class:`Userlist`.
+    """
     pass
 
 
 class SetUserSettings(namedtuple("SetUserSettings",
                                  ["username"] + settings._ALLOWED_MERGES)):
+    """
+    Set settings for the user. :class:`UserSettings` is returned as response.
+
+    See also:
+        :class:`UserSettings`.
+    """
     pass
 
 
 class GetUserSettings(namedtuple("GetUserSettings", ["username"])):
+    """
+    Get settings for given `username`.
+
+    :class:`UserSettings` is returned as response.
+
+    See also:
+        :class:`UserSettings`.
+    """
     pass
 
 
 # Responses ===================================================================
 class Userlist(namedtuple("Userlist", ["users"])):
+    """
+    Response containing names of all users.
+
+    Attributes:
+        users (list): List of registered users.
+    """
     pass
 
 
 class UserSettings(namedtuple("UserSettings",
                               ["username"] + settings._ALLOWED_MERGES)):
+    """
+    All user settings, that user can set himself.
+    """
     pass
 
 
@@ -52,6 +105,18 @@ class ImportRequest(namedtuple("ImportRequest",
                                 "requests",
                                 "import_log",
                                 "error_log"])):
+    """
+    User's import request - mix of files, metadata and metadata-files pairs.
+
+    This request is sent asynchronously when user triggers the upload request.
+
+    Attributes:
+        username (str): Name of the user who sent an import request.
+        requests (list): List of :class:`MetadataFile`/:class:`EbookFile`/
+                         :class:`DataPair` objects.
+        import_log (list of str): Protocol about import.
+        error_log (list of str): Protocol about errors.
+    """
     pass
 
 
@@ -59,6 +124,16 @@ class ImportRequest(namedtuple("ImportRequest",
 class MetadataFile(namedtuple("MetadataFile", ["filename",
                                                "raw_data",
                                                "parsed_data"])):
+    """
+    Structure used to represent Metadata files.
+
+    Attributes:
+        filename (str): Name of the parsed file.
+        raw_data (str): Content of the parsed file.
+        parsed_data (EPublication): :class:`EPublication
+                <edeposit.amqp.aleph.datastructures.epublication.EPublication>`
+                structure.
+    """
     def __new__(self, filename, raw_data=None, parsed_data=None):
         if not raw_data:
             with open(filename) as f:
@@ -72,6 +147,13 @@ class MetadataFile(namedtuple("MetadataFile", ["filename",
         )
 
     def parse(self):
+        """
+        Parse :attr:`raw_data`.
+
+        Returns:
+            MetadataFile: New MetadataFile with parsed data inside (namedtuples
+                          are immutable).
+        """
         return MetadataFile(
             self.filename,
             self.raw_data,
@@ -79,10 +161,17 @@ class MetadataFile(namedtuple("MetadataFile", ["filename",
         )
 
     def get_filenames(self):
+        """
+        Returns:
+            list: of filenames (usually list with one filename).
+        """
         return [self.filename]
 
 
 class EbookFile(namedtuple("EbookFile", ["filename", "raw_data"])):
+    """
+    Structure used to represent data (ebook) files.
+    """
     def __new__(self, filename, raw_data=None):
         if not raw_data:
             with open(filename) as f:
@@ -91,14 +180,35 @@ class EbookFile(namedtuple("EbookFile", ["filename", "raw_data"])):
         return super(EbookFile, self).__new__(self, filename, raw_data)
 
     def get_filenames(self):
+        """
+        Returns:
+            list: of filenames (usually list with one filename).
+        """
         return [self.filename]
 
 
 class DataPair(namedtuple("DataPair", ["metadata_file", "ebook_file"])):
+    """
+    Structure used to repesent MetadataFile - EbookFile pairs.
+
+    Attributes:
+        metadata_file (MetadataFile): Metadata.
+        ebook_file (EbookFile): Data.
+    """
     def parse(self):
+        """
+        Parse metadata file inside.
+
+        Returns:
+            DataPair: New data pair (namedtuples are immutable).
+        """
         return DataPair(self.metadata_file.parse(), self.ebook_file)
 
     def get_filenames(self):
+        """
+        Returns:
+            list: of filenames from objects in this container.
+        """
         return [
             self.metadata_file.filename,  # don't change the order - used in RP
             self.ebook_file.filename
